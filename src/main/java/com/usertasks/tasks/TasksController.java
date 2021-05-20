7dfe1b19dd105d9559702732b53a599ec4be266b
Mahesh Biradar <you@example.com>
2021-05-20 17:01:02 +0530

    Added new demo method to check if all is working for the setup!

--- src/main/java/com/usertasks/tasks/TasksController.java
+++ src/main/java/com/usertasks/tasks/TasksController.java
@@ -1,6 +1,7 @@
 package com.usertasks.tasks;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TasksController {
     @GetMapping(path="/hello-world")
     public String DisplayName(){
         return "hello-World";
+    }
+
+    @GetMapping(path="/hello-world-param/{param}")
+    public String DisplayNameWithParameters(@PathVariable String param){
+        return String.format("This is new parameter %s", param);
     }
 }
