dd63b52c98384917fef483c4cb57b34f361a57d9
Mahesh Biradar <you@example.com>
2021-05-22 14:51:20 +0530

    Added Status Codes for resources!

--- src/main/java/com/usertasks/tasks/Users/UserService.java
+++ src/main/java/com/usertasks/tasks/Users/UserService.java
@@ -1,8 +1,11 @@
 package com.usertasks.tasks.Users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,12 +22,21 @@ public class UserService {
     //GET /user/{id}
     @GetMapping(path = "users/{id}")
     public User getUser(@PathVariable int id) {
-        return userDAO.getUserById(id);
+        User user = userDAO.getUserById(id);
+        if(user == null)
+            throw new UserNotFoundException("invalid user " + id);
+        return user;
     }
 
     //POST /users/{user}
     @PostMapping(path = "/users")
-    public User saveUser(@RequestBody User user) {
-        return userDAO.saveUser(user);
+    public ResponseEntity<Object> saveUser(@RequestBody User user) {
+        User savedUser = userDAO.saveUser(user);
+        URI location = ServletUriComponentsBuilder.
+                fromCurrentRequestUri()
+                .path("/users/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
